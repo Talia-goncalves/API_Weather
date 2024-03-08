7f723072814daf6119988ecb59da4691e3e96d2b
Talia-goncalves <gtalia246@gmail.com>
2024-03-08 15:05:36 -0300

    feat(mongo): connect with mongo

--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from weather.views import WeatherView
+from weather.views import WeatherView, WeatherGenerate
 
 urlpatterns = [
-    path('', WeatherView.as_view()),
+    path('', WeatherView.as_view(), name='Weather View'),
+    path('generate', WeatherGenerate.as_view(), name='Weather Generate'),
 
 ]
